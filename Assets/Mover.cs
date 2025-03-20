dd5f9f8d1e6760c63ad1adf8b60451527399602b
Ismael Busto <ismael.busto@opendeusto.es>
2025-03-20 09:47:00 +0100

    Actualizado con triggers
    nada mas

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -1,8 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObjectMovement : MonoBehaviour
 {
-    public int speed = 5;
+    private Color color = Color.white;
+    public int speed = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,6 @@ public class ObjectMovement : MonoBehaviour
     void Update()
     {
         playerMovement();
-
 
     }
     private void playerMovement()
@@ -35,6 +38,44 @@ public class ObjectMovement : MonoBehaviour
         {
             gameObject.transform.position += new Vector3(0, 0, -speed) * Time.deltaTime;
         };
+
+
+    }
+    //En este caso, la colisión está metida en el player, y se le pone tag a los objetos que se quieran destruir
+    private void OnCollisionEnter(Collision coll) //que empiece a tocar
+    {
+        //forma 1 de hacer colisiones: Utilizando tags
+        if (coll.gameObject.CompareTag("destruible"))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            Destroy(coll.gameObject);
+        }
+    }
+    private void OnCollisionStay(Collision collision) //que siga tocando
+    {
+
+    }
+
+    private void OnCollisionExit(Collision collision) //que acabe de tocar
+    {
+
+    }
+
+    //Colliders con triggers, funcionan de la misma manera que los de arriba, pero el bloque es un "fantasma"
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.CompareTag("destruible"))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+            Destroy(coll.gameObject);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
 
     }
 }
